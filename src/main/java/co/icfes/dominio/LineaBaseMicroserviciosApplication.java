e5aada0fb6d964cf9c594ffa6a5e0ced37a85c2c
Elturcofido <farid.ureche@hotmail.com>
2021-04-16 08:50:36 -0500

    Initial Commit

--- src/main/java/co/icfes/dominio/LineaBaseMicroserviciosApplication.java
+++ src/main/java/co/icfes/dominio/LineaBaseMicroserviciosApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class LineaBaseMicroserviciosApplication {
 
 	public static void main(String[] args) {
--- src/main/java/co/icfes/dominio/audit/AuditAspect.java
+++ src/main/java/co/icfes/dominio/audit/AuditAspect.java
@@ -1,29 +1,34 @@
 package co.icfes.dominio.audit;
 
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-import lombok.extern.slf4j.Slf4j;
 
-//@Aspect
+@Aspect 
 @Component
-@Slf4j
+//@Slf4j
 public class AuditAspect {
-
 	
-//@Around("execution(* co.icfes.dominio.*.*(..))")
-//public Object audit(ProceedingJoinPoint joinPoint) throws Throwable {
-//long t1 = System.currentTimeMillis();
-//Object param1 = joinPoint.getArgs()[0];
-//////log.warn(param1.toString());
-//System.out.println("esto eta ocurriendo" + param1.toString());
-//Object resultado = joinPoint.proceed();
-////
-//return resultado;
-//}
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AuditAspect.class);
 
+
+	@Around("execution(* co.icfes.dominio.controller.*.*(..))")
+	public Object audit(ProceedingJoinPoint joinPoint) throws Throwable {
+		long t1 = System.currentTimeMillis();
+		//Object param1 = joinPoint.getArgs()[0];
+        log.warn("esto eta ocurriendo");
+		
+		Object resultado = joinPoint.proceed();
+
+		return resultado;
+	}
+	
+	 @Before("execution(* consultaId())")
+	  public void log() {
+	    System.out.println("el metodo se ha invocado");
+	  }
 
 }
